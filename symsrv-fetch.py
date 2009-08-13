5795889ff2349fb62e855e4695099ae1ad3bc686
Ted Mielczarek <ted.mielczarek@gmail.com>
2009-08-13 09:30:39 -0400

    First pass * Downloads ATOM feed of latest Windows crashes from Socorro * Processes JSON version of reports, pulls out Module lines * Builds list of modules to look at * Downloads debug symbols from Microsoft Symbol Server using symsrv_convert.exe

--- symsrv-fetch.py
+++ symsrv-fetch.py
@@ -1,44 +1,52 @@
 #
-# This script will read a Socorro database, and try to retrieve
+# This script will read a feed of crash reports from Socorro, and try to retrieve
 # missing symbols from Microsoft's symbol server. It honors a blacklist
 # (blacklist.txt) of symbols that are known to be from our applications,
 # and it maintains its own list of symbols that the MS symbol server
-# doesn't have (skiplist.txt). It also saves the last-run time of the
-# script (last-run.txt) and will only query for reports newer than that
-# time on successive runs.
+# doesn't have (skiplist.txt).
 #
 # The script must have installed alongside it:
-# * The symsrv_convert.exe utility
-# * dbghelp.dll
-# * symsrv.dll
-# * symsrv.yes (a zero-byte file that indicates that you've accepted
-#               the Microsoft symbol server EULA)
-# * config.py  (create this from the template in config.py.in)
+# * msdia80.dll (from the DIA SDK, installed with Visual C++ 8)
+# * dbghelp.dll (from WinDBG)
+# * symsrv.dll  (also from WinDBG)
+# * symsrv.yes  (a zero-byte file that indicates that you've accepted
+#                the Microsoft symbol server EULA)
+# * config.py   (create this from the template in config.py.in)
+#
 # The script also depends on having write access to the directory it is
-# installed in, to write the skiplist/last-run text files. For obvious
-# reasons, the script also needs write access to the symbol_path
-# configured in config.py.
+# installed in, to write the skiplist text file.
 
 import config
 import sys
 import os.path
 import time, datetime
 import subprocess
-import urllib
+import feedparser
+import StringIO
+import gzip
+from collections import defaultdict
+from tempfile import mkdtemp
+from urllib import urlopen
+
+try:
+  import simplejson as json
+except ImportError:
+  import json
 
 # Just hardcoded here
 MICROSOFT_SYMBOL_SERVER = "http://msdl.microsoft.com/download/symbols"
 
-if not os.path.exists(config.symbol_path):
-    print >>sys.stderr, "Symbol path %s doesn't exist" % config.symbol_path
-    sys.exit(1)
+verbose = False
+
+if len(sys.argv) > 1 and sys.argv[1] == "-v":
+  verbose = True
 
 # Symbols that we know belong to us, so don't ask Microsoft for them.
-blacklist=[]
+blacklist=set()
 try:
   bf = file('blacklist.txt', 'r')
   for line in bf:
-      blacklist.append(line.strip())
+      blacklist.add(line.strip())
   bf.close()
 except IOError:
   pass
@@ -58,51 +66,79 @@ except IOError:
   pass
 
 try:
-  f = urllib.urlopen(config.query_data_url)
+  f = feedparser.parse(config.feed_url)
 except IOError:
-  print >>sys.stderr, "Failed to get query data from %s" % config.query_data_url
+  print >>sys.stderr, "Failed to parse feed from %s" % config.feed_url
   sys.exit(1)
 
-# skip the header lines
-f.readline()
-f.readline()
+modules = defaultdict(set)
+symbol_path = mkdtemp()
+# For each crash report in the feed, find the JSON file and parse it,
+# then parse the module list from that data.
+for e in f.entries:
+  for l in e.links:
+    if l.type == 'application/x-gzip':
+      # parse l.href, read modules into modules
+      try:
+        u = urlopen(l.href)
+        data = u.read()
+        u.close()
+        data = gzip.GzipFile(fileobj=StringIO.StringIO(data)).read()
+        j = json.loads(data)
+        u.close()
+        for l in j['dump'].split('\n'):
+          if l.startswith("Module|"):
+            (debugfile, debugid) = l.split('|')[3:5]
+            if debugfile and debugid:
+              modules[debugfile].add(debugid)
+      except IOError:
+        pass
 
-for line in f:
-  if line.startswith('('):
-    break
-  (id, filename) = line.split('|', 1)
-  id = id.strip()
-  filename = filename.strip()
+# Now try to fetch all the unknown modules from the symbol server
+for filename, ids in modules.iteritems():
   if filename in blacklist:
     # This is one of our our debug files from Firefox/Thunderbird/etc
     continue
-  if id in skiplist and skiplist[id] == filename:
-    # We've asked the symbol server previously about this, so skip it.
-    continue
-  sym_file = os.path.join(config.symbol_path, filename, id,
-                          filename.replace(".pdb","") + ".sym")
-  if os.path.exists(sym_file):
-    # We already have this symbol
-    continue
-  if config.read_only_symbol_path != '' and \
-     os.path.exists(os.path.join(config.symbol_path, filename, id,
-                                 filename.replace(".pdb","") + ".sym")):
-    # We already have this symbol
-    continue
-  # Not in the blacklist, skiplist, and we don't already have it, so
-  # ask the symbol server for it.
-  # This expects that symsrv_convert.exe and all its dependencies
-  # are in the current directory.
-  rv = subprocess.call(["symsrv_convert.exe",
-                        MICROSOFT_SYMBOL_SERVER,
-                        config.symbol_path,
-                        filename,
-                        id])
-  # Return code of 2 or higher is an error
-  if rv >= 2:
-    skiplist[id] = filename
-  # Otherwise we just assume it was written out, not much we can do
-  # if it wasn't. We'll try again next time we see it anyway.
+  for id in ids:
+    if id in skiplist and skiplist[id] == filename:
+      # We've asked the symbol server previously about this, so skip it.
+      continue
+    sym_file = os.path.join(symbol_path, filename, id,
+                            filename.replace(".pdb","") + ".sym")
+    if os.path.exists(sym_file):
+      # We already have this symbol
+      continue
+    if config.read_only_symbol_path != '' and \
+       os.path.exists(os.path.join(config.read_only_symbol_path, filename, id,
+                                   filename.replace(".pdb","") + ".sym")):
+      # We already have this symbol
+      continue
+    # Not in the blacklist, skiplist, and we don't already have it, so
+    # ask the symbol server for it.
+    # This expects that symsrv_convert.exe and all its dependencies
+    # are in the current directory.
+    if not verbose:
+      stdout = open("NUL","w")
+      stderr = open("NUL","w")
+    else:
+      stdout = None
+      stderr = None
+      print "fetching %s %s" % (filename, id)
+    rv = subprocess.call(["symsrv_convert.exe",
+                          MICROSOFT_SYMBOL_SERVER,
+                          symbol_path,
+                          filename,
+                          id],
+                         stdout = stdout,
+                         stderr = stderr)
+    # Return code of 2 or higher is an error
+    if rv >= 2:
+      skiplist[id] = filename
+    # Otherwise we just assume it was written out, not much we can do
+    # if it wasn't. We'll try again next time we see it anyway.
+
+#XXX: upload everything from symbol_path, and then delete the directory
+#shutil.rmtree(symbol_path)
 
 # Write out our new skip list
 try:
