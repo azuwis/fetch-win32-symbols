3b6aba07645715b3c2d5e20e36fc9d862eb44809
Ted Mielczarek <ted.mielczarek@gmail.com>
2012-09-05 09:35:02 -0400

    Don't skiplist symbols if we have previously downloaded another version of symbols for the same file.

--- symsrv-fetch.py
+++ symsrv-fetch.py
@@ -36,6 +36,16 @@ from urllib import urlopen
 # Just hardcoded here
 MICROSOFT_SYMBOL_SERVER = "http://msdl.microsoft.com/download/symbols"
 
+thisdir = os.path.dirname(__file__)
+
+def write_skiplist():
+  try:
+    with open(os.path.join(thisdir, 'skiplist.txt'), 'w') as sf:
+      for (debug_id,debug_file) in skiplist.iteritems():
+          sf.write("%s %s\n" % (debug_id, debug_file))
+  except IOError:
+    log.exception("Error writing skiplist.txt")
+
 verbose = False
 if len(sys.argv) > 1 and sys.argv[1] == "-v":
   verbose = True
@@ -44,7 +54,6 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 formatter = logging.Formatter(fmt="%(asctime)-15s %(message)s",
                               datefmt="%Y-%m-%d %H:%M:%S")
-thisdir = os.path.dirname(__file__)
 filelog = logging.FileHandler(filename=os.path.join(thisdir,
                                                     "symsrv-fetch.log"))
 filelog.setLevel(logging.INFO)
@@ -115,6 +124,7 @@ current = 0
 blacklist_count = 0
 skiplist_count = 0
 existing_count = 0
+not_found_count = 0
 file_index = []
 # Now try to fetch all the unknown modules from the symbol server
 for filename, ids in modules.iteritems():
@@ -135,6 +145,7 @@ for filename, ids in modules.iteritems():
                                                     filename[:20]))
     if id in skiplist and skiplist[id] == filename.lower():
       # We've asked the symbol server previously about this, so skip it.
+      log.debug("%s/%s already in skiplist", filename, id)
       skiplist_count += 1
       continue
     rel_path = os.path.join(filename, id,
@@ -142,12 +153,14 @@ for filename, ids in modules.iteritems():
     sym_file = os.path.join(symbol_path, rel_path)
     if os.path.exists(sym_file):
       # We already have this symbol
+      log.debug("%s/%s already present", filename, id)
       existing_count += 1
       continue
     if config.read_only_symbol_path != '' and \
        os.path.exists(os.path.join(config.read_only_symbol_path, filename, id,
                                    filename.replace(".pdb","") + ".sym")):
       # We already have this symbol
+      log.debug("%s/%s already present", filename, id)
       existing_count += 1
       continue
     # Not in the blacklist, skiplist, and we don't already have it, so
@@ -171,20 +184,31 @@ for filename, ids in modules.iteritems():
       time.sleep(1)
     if proc.poll() is None:
       # kill it, it's been too long
+      log.debug("Timed out downloading %s/%s", filename, id)
       ctypes.windll.kernel32.TerminateProcess(int(proc._handle), -1)
     # Return code of 2 or higher is an error
     elif proc.returncode >= 2:
-      skiplist[id] = filename
-      skiplist_count += 1
+      not_found_count += 1
+      # Don't skiplist this symbol if we've previously downloaded
+      # other symbol versions for the same file. It's likely we'll
+      # be able to download it at some point
+      if not (os.path.exists(os.path.join(symbol_path, filename)) or
+              os.path.exists(os.path.join(config.read_only_symbol_path, filename))):
+        log.debug("Couldn't fetch %s/%s, adding to skiplist", filename, id)
+        skiplist[id] = filename
+      else:
+        log.debug("Couldn't fetch %s/%s, but not skiplisting", filename, id)
     if os.path.exists(sym_file):
+      log.debug("Successfully downloaded %s/%s", filename, id)
       file_index.append(rel_path.replace("\\", "/"))
 
 if verbose:
   sys.stdout.write("\n")
 
 if not file_index:
-  log.info("No symbols downloaded: %d considered, %d already present, %d in blacklist, %d skipped"
-           % (total, existing_count, blacklist_count, skiplist_count))
+  log.info("No symbols downloaded: %d considered, %d already present, %d in blacklist, %d skipped, %d not found"
+           % (total, existing_count, blacklist_count, skiplist_count, not_found_count))
+  write_skiplist()
   sys.exit(0)
 
 # Write an index file
@@ -231,13 +255,7 @@ finally:
   shutil.rmtree(symbol_path, True)
 
 # Write out our new skip list
-try:
-  sf = file(os.path.join(thisdir, 'skiplist.txt'), 'w')
-  for (debug_id,debug_file) in skiplist.iteritems():
-      sf.write("%s %s\n" % (debug_id, debug_file))
-  sf.close()
-except IOError:
-  log.exception("Error writing skiplist.txt")
+write_skiplist()
 
 log.info("Uploaded %d symbol files" % len(file_index))
 log.info("Finished, exiting")
