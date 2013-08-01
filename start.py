ad8af2a8c7057836252b9e3c598fd4df0148dca0
Zhong Jianxin <azuwis@gmail.com>
2013-08-01 15:41:43 +0800

    Fix symbols.csv generating and add comments

--- start.py
+++ start.py
@@ -12,6 +12,7 @@ import config
 this_dir = os.path.dirname(__file__)
 timestamp = os.path.join(this_dir, "timestamp")
 
+# reset symbols csv file
 open(config.csv_url, "w").close()
 
 if not os.path.exists(timestamp):
@@ -22,18 +23,22 @@ else:
 
 most_recent = last_processed
 
+# glob .jsonz of the current month
 jsonzs = glob.glob("%s/%s*/name/*/*/*.jsonz" % (config.processed_crash, datetime.now().strftime("%Y%m")))
 
-for jsonz in jsonzs:
-    this_mtime = os.path.getmtime(jsonz)
-    if this_mtime <= last_processed:
-        continue
-    if this_mtime > most_recent:
-        most_recent = this_mtime
-    dump = json.load(gzip.open(jsonz))["dump"].split("\n")
-    reader = csv.reader(dump, delimiter='|', quoting=csv.QUOTE_NONE)
-    with open(config.csv_url, "wb") as csvfile:
-        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_NONE)
+with open(config.csv_url, "wb") as csvfile:
+    csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_NONE)
+    # parse all new jsonz, and save modules to csv file
+    for jsonz in jsonzs:
+        this_mtime = os.path.getmtime(jsonz)
+        if this_mtime <= last_processed:
+            continue
+        if this_mtime > most_recent:
+            most_recent = this_mtime
+        crash = json.load(gzip.open(jsonz))
+
+        dump = crash["dump"].split("\n")
+        reader = csv.reader(dump, delimiter='|', quoting=csv.QUOTE_NONE)
         for row in reader:
             if (len(row) > 4 and row[0] == "Module"):
                 csvwriter.writerow([row[1], row[3], row[4]])
